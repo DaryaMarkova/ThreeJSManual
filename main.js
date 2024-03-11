c6166a69d7e771e365d916338099ffc0e356f4b4
DaryaMarkova <darya.towarddreams@yandex.ru>
2024-03-11 06:38:07 +0300

    modifying compability

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 import * as THREE from "three";
+import WebGL from "three/addons/capabilities/WebGL.js";
+
+if (WebGL.isWebGLAvailable()) {
+  console.log("webgl available");
+} else {
+  console.log("webgl permitted");
+}
